b10b92e1698ed0e18da0c9e5cfb89485ff434d7b
--replace-all <omarsamirelmokadem@gmail.com>
2025-12-24 01:55:05 +0300

    implement health check endpoint in main.go

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("First Program")
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "STATUS: OK")
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
